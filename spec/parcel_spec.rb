21827fc7b7e38cd95718a01ef2cdd026d1311104
Alyssa Horrocks & Justin Scott <student@epicodus.com>
2015-08-10 12:09:52 -0700

    Have second spec returning the cost to ship

--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -7,6 +7,11 @@ describe(Parcel) do
       test_parcel = Parcel.new(2, 2, 2)
       expect(test_parcel.volume()).to(eq(8))
     end
+
+    it('will return the amount of the cost to ship') do
+      test_parcel = Parcel.new(2, 2, 2)
+      expect(test_parcel.cost_to_ship()).to(eq())
+    end
   end
-  
+
 end
